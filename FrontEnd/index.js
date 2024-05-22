8c1a1246da850cbd31125108f21f487a1801d82d
Anais Marmuse <marmuseanais@gmail.com>
2024-05-22 20:06:22 +0200

    filtres

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,27 +1,84 @@
-/*Variables*/
+/**Variables*/
 const gallery = document.getElementById("galerie");
+const filtres = document.getElementById("filtres");
 
-
-/* fonction qui retourne le tableau des travaux provenant du back-end*/
+/* * fonction qui retourne le tableau des travaux provenant du back-end*/
 async function recupererTravaux() {
-    const donnees = await fetch("http://localhost:5678/api/works");
-    return await donnees.json();
+  const donnees = await fetch("http://localhost:5678/api/works");
+  return await donnees.json();
 }
-recupererTravaux();
 
+/**fonction-Affiche travaux dans la galerie*/
+function creationProjet(projet) {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  img.src = projet.imageUrl;
+  figcaption.textContent = projet.title;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
 
-/*fonction-Affiche travaux dans la galerie*/
-async function affichageGalerie(){
-    const tableauTravaux= await recupererTravaux();
-    tableauTravaux.forEach(element => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
-        img.src = element.imageURL;
-        figcaption.textContent = element.title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-    });
+async function affichageGalerie() {
+  const tableauTravaux = await recupererTravaux();
+  tableauTravaux.forEach((projet) => {
+    creationProjet(projet);
+  });
 }
 affichageGalerie();
+
+/**
+*tableaux catégorie
+ */
+async function recupererCategorie () {
+  const reponse = await fetch ("http://localhost:5678/api/categories");
+  return await reponse.json();
+}
+
+/**
+ *Afficher les boutons 
+ */
+async function afficherBoutons() {
+  const categories = await recupererCategorie();
+  console.log(categories)
+  const btnTous = document.createElement("button");
+  btnTous.textContent ="Tous";
+  btnTous.id = "0";
+  btnTous.classList.add("filtres");
+  filtres.appendChild(btnTous);
+  categories.forEach(category => {
+    const btn = document.createElement("button");
+    btn.textContent = category.name;
+    btn.id = category.id;
+    btn.classList.add("filtres")
+    filtres.appendChild(btn)
+  });
+}
+afficherBoutons();
+
+/**
+ * filtre au clique
+ */
+async function filtrerCategories() {
+  const projets = await recupererTravaux();
+  const boutons = document.querySelectorAll("button");
+  console.log(boutons);
+  boutons.forEach(bouton => {
+    bouton.addEventListener("click",(e)=>{
+      const boutonId = e.target.id;
+      gallery.innerHTML="";
+      if (boutonId !== "0") {
+        const projetFiltre = projets.filter((projet)=>{
+          return projet.categoryId == boutonId;
+        });
+        projetFiltre.forEach(projet => {
+          creationProjet(projet);
+        });
+      }else{
+        affichageGalerie();
+      }
+    });
+  });
+}
+filtrerCategories();
